4d81c5854baacb6c160c1e49d18cd714d0ddbe32
faridnizam <faridsaja12@gmail.com>
2021-12-02 16:53:42 +0700

    add webhook

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const app = require('./src/app');
 const color = require('./src/utils/color-log');
+const webhook = require('./discord');
 
 const PORT = process.env.PORT || 5400;
 const localhost = '127.0.0.1';
 
 app.listen(PORT, () => {
-  console.log(color.cyan, `Server listening on http://${localhost}:${PORT}`);
+  if (process.env.NODE_ENV !== 'production') {
+    console.log(color.cyan, `Server listening on http://${localhost}:${PORT}`);
+  } else {
+    console.log(`Server listening on http://${localhost}:${PORT}`);
+    webhook.info(`Server listening on http://${localhost}:${PORT}`);
+  }
+
+  // if (process.env.NODE_ENV == 'production') {
+  //   console.log(`Server listening on http://${localhost}:${PORT}`);
+  //   webhook.info(`Server listening on http://${localhost}:${PORT}`);
+  // }
 });
